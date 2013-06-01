a1f42b20c1d00e120a7d6ec22b88bae62bc16fa1
pavel <pavel@b9f8e3bb-e037-431a-9fd7-e95126a52262>
2013-06-01 16:17:22 +0000

    add EG library to examples/DelphesCMSFWLite.sh

--- examples/DelphesCMSFWLite.sh
+++ examples/DelphesCMSFWLite.sh
@@ -3,5 +3,5 @@ INC="`echo -I$CMSSW_FWLITE_INCLUDE_PATH | sed 's/:/ -I/g'` -I$CMSSW_RELEASE_BASE
 
 LIB="`echo -L$LD_LIBRARY_PATH | sed 's/include/lib/g; s/:/ -L/g'` `echo -L$CMSSW_FWLITE_INCLUDE_PATH | sed 's/include/lib/g; s/:/ -L/g'` -lFWCoreFWLite -lDataFormatsFWLite -lDataFormatsPatCandidates -lDataFormatsLuminosity -lCommonToolsUtils"
 
-g++ -std=c++0x -I. -Iexternal $INC `root-config --cflags --ldflags --libs` -L. -lDelphes $LIB -o DelphesCMSFWLite examples/DelphesCMSFWLite.cpp
+g++ -std=c++0x -I. -Iexternal $INC `root-config --cflags --ldflags --libs` -lEG -L. -lDelphes $LIB -o DelphesCMSFWLite examples/DelphesCMSFWLite.cpp
 
