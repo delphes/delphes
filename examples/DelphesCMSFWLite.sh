94dcf80504dd9b798848d330752e74462bdda651
pavel <pavel@b9f8e3bb-e037-431a-9fd7-e95126a52262>
2013-07-04 11:02:10 +0000

    remove DelphesCMSFWLite.sh and DelphesProMC.sh

--- examples/DelphesCMSFWLite.sh
+++
@@ -1,8 +0,0 @@
-echo ">> Building DelphesCMSFWLite"
-
-INC="`echo -I$CMSSW_FWLITE_INCLUDE_PATH | sed 's/:/ -I/g'` -I$CMSSW_RELEASE_BASE/src"
-
-LIB="`echo -L$LD_LIBRARY_PATH | sed 's/include/lib/g; s/:/ -L/g'` `echo -L$CMSSW_FWLITE_INCLUDE_PATH | sed 's/include/lib/g; s/:/ -L/g'` -lFWCoreFWLite -lDataFormatsFWLite -lDataFormatsPatCandidates -lDataFormatsLuminosity -lCommonToolsUtils"
-
-g++ -std=c++0x -I. -Iexternal $INC `root-config --cflags --ldflags --libs` -lEG -L. -lDelphes $LIB -o DelphesCMSFWLite examples/DelphesCMSFWLite.cpp
-
