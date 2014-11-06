5e6db0872b5d5e03955b5b5a6fcf447f5a963382
Christophe Delaere <christophe.delaere@cern.ch>
2014-11-06 17:07:16 +0100

    Minor cleaning in script

--- examples/EventDisplay.C
+++ examples/EventDisplay.C
@@ -1,3 +1,6 @@
+/* Example:
+ * root -l 'evdisplay.C("delphes_card_CMS.tcl","../delphes_output.root")'
+ */
 
 void evdisplay(const char* configfile = "delphes_card_CMS.tcl", const char* datafile = "delphes_output.root",
                const char* ParticlePropagator="ParticlePropagator",
@@ -14,21 +17,7 @@ void evdisplay(const char* configfile = "delphes_card_CMS.tcl", const char* data
    Delphes3DGeometry det3D(new TGeoManager("delphes", "Delphes geometry"));
    det3D.readFile(configfile, ParticlePropagator, TrackingEfficiency, MuonEfficiency, Calorimeters);
 
-   // create the application items
+   // create the application
    DelphesEventDisplay* display = new DelphesEventDisplay(configfile, datafile, det3D);
-
-/*
-   // EClipType not exported to CINT (see TGLUtil.h):
-   // 0 - no clip, 1 - clip plane, 2 - clip box
-   TGLViewer *v = gEve->GetDefaultGLViewer();
-   //Double_t plane[4] = { 0., 1., 0., 0. };
-   //v->GetClipSet()->SetClipState(1,plane);
-   //v->GetClipSet()->SetClipType(1);
-   //v->ColorSet().Background().SetColor(kMagenta+4);
-   //v->SetGuideState(TGLUtil::kAxesEdge, kTRUE, kFALSE, 0);
-   v->RefreshPadEditor(v);
-   v->CurrentCamera().RotateRad(-1.2, 0.5);
-   v->DoDraw();
-*/
 }
 
