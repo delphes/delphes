1fa50c28409a44b952b4b2f1a0c4f30a60f8cc50
Pavel Demin <pavel.demin@cern.ch>
2014-12-14 00:41:59 +0100

    fix GPLv3 header

--- doc/prepend_GPLv3_header.sh
+++ doc/prepend_GPLv3_header.sh
@@ -5,7 +5,7 @@ length=`wc -l $header | cut -d' ' -f1`
 
 for file in `find . -maxdepth 2 -type f -name *.cpp -o -name *.cc -o -name *.h`
 do
-  head -$length $file | diff -l $header - > /dev/null && continue
+  head -$length $file | diff -lb $header - > /dev/null && continue
   echo $file
   cat $header $file > $file.new
   mv $file.new $file
--- modules/AngularSmearing.cc
+++ modules/AngularSmearing.cc
@@ -1,17 +1,17 @@
 /*
  *  Delphes: a framework for fast simulation of a generic collider experiment
  *  Copyright (C) 2012-2014  Universite catholique de Louvain (UCL), Belgium
- *  
+ *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -22,7 +22,7 @@
  *  Performs transverse angular resolution smearing.
  *
  *  $Date: 2014-06-17 16:58:53 +0100  $
- *  
+ *
  *
  *
  *  \author M. Selvaggi - UCL, Louvain-la-Neuve
