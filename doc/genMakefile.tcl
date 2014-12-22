a24426f6bbe2b0c48be2099a145819fa235aea87
Pavel Demin <pavel.demin@cern.ch>
2014-12-22 17:33:17 +0100

    add CMakeLists.txt, DelphesEnv.sh, README_4LHCb and MinBias.pileup to tar file

--- doc/genMakefile.tcl
+++ doc/genMakefile.tcl
@@ -395,7 +395,7 @@ distclean: clean
 dist:
 	@echo ">> Building $(DISTTAR)"
 	@mkdir -p $(DISTDIR)
-	@cp -a CHANGELOG COPYING CREDITS README VERSION Makefile configure cards classes converters display doc examples external modules python readers $(DISTDIR)
+	@cp -a CHANGELOG CMakeLists.txt COPYING CREDITS DelphesEnv.sh README README_4LHCb VERSION Makefile MinBias.pileup configure cards classes converters display doc examples external modules python readers $(DISTDIR)
 	@find $(DISTDIR) -depth -name .\* -exec rm -rf {} \;
 	@tar -czf $(DISTTAR) $(DISTDIR)
 	@rm -rf $(DISTDIR)
