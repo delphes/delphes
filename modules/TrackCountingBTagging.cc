93af22b46431b5e63cafae669845cbdccf891121
Michele Selvaggi <michele.selvaggi@cern.ch>
2015-09-29 18:49:24 +0200

    compute abs value of dxy as sqrt(xd*xd + yd*yd)

--- modules/TrackCountingBTagging.cc
+++ modules/TrackCountingBTagging.cc
@@ -125,7 +125,7 @@ void TrackCountingBTagging::Process()
 
       xd = track->Xd;
       yd = track->Yd;
-      dxy = TMath::Abs(track->Dxy);
+      dxy = TMath::Sqrt(xd*xd + yd*yd);
       ddxy = track->SDxy;
 
       if(tpt < fPtMin) continue;
