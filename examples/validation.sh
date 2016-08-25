629e819d7e6e221169d05d4e06d9feb1c70d2aac
Alexandre Mertens <alexandre.mertens@cern.ch>
2016-08-25 16:29:55 +0200

    some bug fixes

--- examples/validation.sh
+++ examples/validation.sh
@@ -12,11 +12,6 @@ runParticleGun ph 22
 runParticleGun b 5
 runParticleGun jet 1
 runParticleGun tau 15
-exit
-# hadd delphes_all.root delphes_ParticleGunmu.root delphes_ParticleGunb.root delphes_ParticleGunjet.root
-hadd delphes_b_jet.root delphes_ParticleGunb.root delphes_ParticleGuntau.root delphes_ParticleGunjet.root
 
-./Validation_electron delphes_ParticleGunel.root delphes_validation_electron.root
-./Validation_muon delphes_ParticleGunmu.root delphes_validation_muon.root
-./Validation_photon delphes_ParticleGunph.root delphes_validation_photon.root
-./Validation_jet delphes_b_tau_jet.root delphes_validation_jet.root
+./Validation delphes_ParticleGunel.root delphes_ParticleGunmu.root delphes_ParticleGunph.root delphes_ParticleGunjet.root delphes_ParticleGunb.root delphes_ParticleGuntau.root validation.root
+
