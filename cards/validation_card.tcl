4d128b95e33cd5c2cedc47194fd7baa586d1a26a
Michele Selvaggi <michele.selvaggi@cern.ch>
2016-09-30 14:42:43 +0200

    fixed validation card for compliance with all cards

--- cards/validation_card.tcl
+++ cards/validation_card.tcl
@@ -2,7 +2,9 @@ source delphes_card_CMS.tcl
 
 set ExecutionPath [lreplace $ExecutionPath end end]
 add ExecutionPath CaloJetFinder
+add ExecutionPath PFJetFinder
 add ExecutionPath CaloMissingET
+add ExecutionPath PFMissingET
 add ExecutionPath GenScalarHT
 add ExecutionPath PionFilter
 add ExecutionPath TreeWriter
@@ -26,7 +28,23 @@ module FastJetFinder CaloJetFinder {
 
   # algorithm: 1 CDFJetClu, 2 MidPoint, 3 SIScone, 4 kt, 5 Cambridge/Aachen, 6 antikt
   set JetAlgorithm 6
-  set ParameterR 0.5
+  set ParameterR 0.4
+
+  set JetPTMin 1.0
+}
+
+########################
+# PFJet finder
+########################
+
+module FastJetFinder PFJetFinder {
+  set InputArray EFlowMerger/eflow
+
+  set OutputArray jets
+
+  # algorithm: 1 CDFJetClu, 2 MidPoint, 3 SIScone, 4 kt, 5 Cambridge/Aachen, 6 antikt
+  set JetAlgorithm 6
+  set ParameterR 0.4
 
   set JetPTMin 1.0
 }
@@ -40,6 +58,17 @@ module Merger CaloMissingET {
   add InputArray Calorimeter/towers
   set MomentumOutputArray momentum
 }
+
+#########################
+# Calo Missing ET merger
+########################
+
+module Merger PFMissingET {
+# add InputArray InputArray
+  add InputArray EFlowMerger/eflow
+  set MomentumOutputArray momentum
+}
+
 
 #################
 # Gen Scalar HT
@@ -66,7 +95,9 @@ module PdgCodeFilter PionFilter {
 module TreeWriter TreeWriter {
 # add Branch InputArray BranchName BranchClass
   add Branch CaloJetFinder/jets CaloJet Jet
+  add Branch PFJetFinder/jets PFJet Jet
   add Branch CaloMissingET/momentum CaloMissingET MissingET
+  add Branch PFMissingET/momentum PFMissingET MissingET
   add Branch GenScalarHT/energy GenScalarHT ScalarHT
   add Branch PionFilter/pions Pion Track 
   add Branch ElectronFilter/electrons ElectronPF Electron 
