1fe849c6b1857e7b331cbb0dc4cdbfc6ebb56729
Michele Selvaggi <michele.selvaggi@cern.ch>
2017-05-16 22:28:17 +0200

    Update CMS_PhaseII_Substructure_PIX4022_0PU.tcl

--- cards/CMS_PhaseII/CMS_PhaseII_Substructure_PIX4022_0PU.tcl
+++ cards/CMS_PhaseII/CMS_PhaseII_Substructure_PIX4022_0PU.tcl
@@ -133,7 +133,7 @@ module PileUpMerger PileUpMerger {
   set VertexOutputArray vertices
 
   # pre-generated minbias input file
-  set PileUpFile ../eos/cms/store/group/upgrade/delphes/PhaseII/MinBias_100k.pileup
+  set PileUpFile /eos/cms/store/group/upgrade/delphes/PhaseII/MinBias_100k.pileup
   #set PileUpFile MinBias.pileup
 
   # average expected pile up
