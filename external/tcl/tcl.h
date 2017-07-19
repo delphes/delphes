9e55065b12f7a7c4c76645510981e68efe51878a
Pavel Demin <pavel.demin@cern.ch>
2017-07-19 15:39:55 +0200

    replace string[4] with string[1] in tcl.h

--- external/tcl/tcl.h
+++ external/tcl/tcl.h
@@ -794,7 +794,7 @@ typedef struct Tcl_HashEntry {
 					 * The actual size will be as large
 					 * as necessary for this table's
 					 * keys. */
-	char string[4];			/* String for key.  The actual size
+	char string[1];			/* String for key.  The actual size
 					 * will be as large as needed to hold
 					 * the key. */
     } key;				/* MUST BE LAST FIELD IN RECORD!! */
