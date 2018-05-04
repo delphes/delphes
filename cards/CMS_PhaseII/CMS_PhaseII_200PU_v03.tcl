0a122b56126288e81671eec899dc9aa6b5065132
Luca Cadamuro <lc.cadamuro@gmail.com>
2018-05-04 10:18:43 +0200

    Revert to default pileup file

--- cards/CMS_PhaseII/CMS_PhaseII_200PU_v03.tcl
+++ cards/CMS_PhaseII/CMS_PhaseII_200PU_v03.tcl
@@ -132,9 +132,8 @@ module PileUpMerger PileUpMerger {
   set VertexOutputArray vertices
 
   # pre-generated minbias input file
-  # set PileUpFile /eos/cms/store/group/upgrade/delphes/PhaseII/MinBias_100k.pileup
+  set PileUpFile /eos/cms/store/group/upgrade/delphes/PhaseII/MinBias_100k.pileup
   #set PileUpFile MinBias.pileup
-  set PileUpFile MinBias_100k.pileup
 
   # average expected pile up
   set MeanPileUp 200
