5af205ec28c7fdebc130c6a742d5ff4a6f0e28b9
Thijs Miedema <mail@tmiedema.com>
2021-05-27 22:34:24 +0200

    Update DelphesEnv.sh

--- DelphesEnv.sh
+++ DelphesEnv.sh
@@ -2,5 +2,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 export DELPHES_HOME="$DIR"
 export PYTHONPATH="$DIR/python:${PYTHONPATH}"
-export LD_LIBRARY_PATH="$DIR:${LD_LIBARARY_PATH}"
+export LD_LIBRARY_PATH="$DIR:${LD_LIBRARY_PATH}"
 export LIBRARY_PATH="$DIR:${LIBRARY_PATH}"
