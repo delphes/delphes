dfdad8b71bac6b2f7c248d2a756d134cbee04cd8
Thijs Miedema <mail@tmiedema.com>
2021-05-28 07:13:24 +0200

    Make DelphesEnv relative to the file instead of the pwd. (#98)
    * Make DelphesEnv relative to the file instead of the pwd.

--- DelphesEnv.sh
+++ DelphesEnv.sh
@@ -1,3 +1,6 @@
-export PYTHONPATH=`pwd`/python:$PYTHONPATH
-export LD_LIBRARY_PATH=`pwd`:$LD_LIBRARY_PATH
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
+export DELPHES_HOME="$DIR"
+export PYTHONPATH="$DIR/python:${PYTHONPATH}"
+export LD_LIBRARY_PATH="$DIR:${LD_LIBRARY_PATH}"
+export LIBRARY_PATH="$DIR:${LIBRARY_PATH}"
