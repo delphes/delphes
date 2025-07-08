7a7f286a25e70459b336b20f4ea68641485a2743
Jan Tuzlic Offermann <offermannjan@yahoo.com>
2025-07-08 10:33:12 +0200

    updating tracking resolution to include |p|, cot(theta) and phi resolutions based on CMS Tracker TDR, Phase-I

--- cards/trackResolutionCMS.tcl
+++ cards/trackResolutionCMS.tcl
@@ -1,6 +1,148 @@
-set PResolutionFormula { 0.0 }
-set CtgThetaResolutionFormula { 0.0 }
-set PhiResolutionFormula { 0.0 }
+# I've adjusted the resolution formulae for P, CtgTheta and Phi to not be zero,
+# otherwise this totally breaks the VertexFinderDA4D since it computes weighted
+# positions that incorporate these errors (and having them be zero will result in
+# NaNs). - Jan T. Offermann, Brown University
+
+# Based on CMS Tracker TDR: https://cds.cern.ch/record/368412/. Data is extracted
+# from the cited figures using the WebPlotDigitizer tool. Corresponds to Phase-I.
+
+# See Figure 9.10, pg 484.
+# Here, I'm using the pT resolution from the CMS Tracker TDR, together with
+# the ctgTheta resolution (cot(theta) = sinh(eta)) that's given further below.
+# (Note: These formula are passed to the DelphesFormula class, which is
+#  an extension of the ROOT TFormula class).
+
+# deta = d(arcsinh(cot(theta))) = 1 / sqrt(ctgTheta^2 + 1) * d(ctgTheta)
+# p = pt * cosh(eta)
+# => dp = d(pt * cosh(eta)) = \sqrt{ (pt * sinh(eta))^2 * deta^2 + cosh^2(eta) * dpt^2 }
+# => dp^2 = pt^2 * sinh^2(eta) * deta^2 + cosh^2(eta) * dpt^2
+#         = pt^2 * sinh^2(eta) / (ctgTheta^2 + 1) * d(ctgTheta)^2 + cosh^2(eta) * dpt^2
+#         = pt^2 * sinh^2(eta) / (sinh^2(eta) + 1) * d(ctgTheta)^2 + cosh^2(eta) * dpt^2
+
+# TODO: Double-check all this. From at least initial tests, it seems reasonable.
+set PResolutionFormula {
+    ( abs(eta) > 0.0 && abs(eta) <= 0.2 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0022)**2 + TMath::CosH(eta)**2 * (0.0066)**2 ) +\
+    ( abs(eta) > 0.2 && abs(eta) <= 0.4 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0021)**2 + TMath::CosH(eta)**2 * (0.0069)**2 ) +\
+    ( abs(eta) > 0.4 && abs(eta) <= 0.6 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0022)**2 + TMath::CosH(eta)**2 * (0.0070)**2 ) +\
+    ( abs(eta) > 0.6 && abs(eta) <= 0.8 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0025)**2 + TMath::CosH(eta)**2 * (0.0068)**2 ) +\
+    ( abs(eta) > 0.8 && abs(eta) <= 1.0 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0030)**2 + TMath::CosH(eta)**2 * (0.0079)**2 ) +\
+    ( abs(eta) > 1.0 && abs(eta) <= 1.2 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0038)**2 + TMath::CosH(eta)**2 * (0.0101)**2 ) +\
+    ( abs(eta) > 1.2 && abs(eta) <= 1.4 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0049)**2 + TMath::CosH(eta)**2 * (0.0121)**2 ) +\
+    ( abs(eta) > 1.4 && abs(eta) <= 1.6 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0064)**2 + TMath::CosH(eta)**2 * (0.0123)**2 ) +\
+    ( abs(eta) > 1.6 && abs(eta) <= 1.8 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0080)**2 + TMath::CosH(eta)**2 * (0.0130)**2 ) +\
+    ( abs(eta) > 1.8 && abs(eta) <= 2.0 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0112)**2 + TMath::CosH(eta)**2 * (0.0144)**2 ) +\
+    ( abs(eta) > 2.0 && abs(eta) <= 2.2 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0145)**2 + TMath::CosH(eta)**2 * (0.0154)**2 ) +\
+    ( abs(eta) > 2.2 && abs(eta) <= 2.4 ) * ( pt <= 5 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0170)**2 + TMath::CosH(eta)**2 * (0.0153)**2 ) +\
+    ( abs(eta) > 0.0 && abs(eta) <= 0.2 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0008)**2 + TMath::CosH(eta)**2 * (0.0063)**2 ) +\
+    ( abs(eta) > 0.2 && abs(eta) <= 0.4 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0007)**2 + TMath::CosH(eta)**2 * (0.0063)**2 ) +\
+    ( abs(eta) > 0.4 && abs(eta) <= 0.6 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0005)**2 + TMath::CosH(eta)**2 * (0.0067)**2 ) +\
+    ( abs(eta) > 0.6 && abs(eta) <= 0.8 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0006)**2 + TMath::CosH(eta)**2 * (0.0066)**2 ) +\
+    ( abs(eta) > 0.8 && abs(eta) <= 1.0 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0007)**2 + TMath::CosH(eta)**2 * (0.0077)**2 ) +\
+    ( abs(eta) > 1.0 && abs(eta) <= 1.2 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0009)**2 + TMath::CosH(eta)**2 * (0.0084)**2 ) +\
+    ( abs(eta) > 1.2 && abs(eta) <= 1.4 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0010)**2 + TMath::CosH(eta)**2 * (0.0093)**2 ) +\
+    ( abs(eta) > 1.4 && abs(eta) <= 1.6 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0011)**2 + TMath::CosH(eta)**2 * (0.0103)**2 ) +\
+    ( abs(eta) > 1.6 && abs(eta) <= 1.8 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0011)**2 + TMath::CosH(eta)**2 * (0.0108)**2 ) +\
+    ( abs(eta) > 1.8 && abs(eta) <= 2.0 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0013)**2 + TMath::CosH(eta)**2 * (0.0131)**2 ) +\
+    ( abs(eta) > 2.0 && abs(eta) <= 2.2 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0015)**2 + TMath::CosH(eta)**2 * (0.0150)**2 ) +\
+    ( abs(eta) > 2.2 && abs(eta) <= 2.4 ) * ( pt > 5. && pt <= 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0024)**2 + TMath::CosH(eta)**2 * (0.0166)**2 ) +\
+    ( abs(eta) > 0.0 && abs(eta) <= 0.2 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0008)**2 + TMath::CosH(eta)**2 * (0.0116)**2 ) +\
+    ( abs(eta) > 0.2 && abs(eta) <= 0.4 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0006)**2 + TMath::CosH(eta)**2 * (0.0135)**2 ) +\
+    ( abs(eta) > 0.4 && abs(eta) <= 0.6 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0005)**2 + TMath::CosH(eta)**2 * (0.0121)**2 ) +\
+    ( abs(eta) > 0.6 && abs(eta) <= 0.8 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0006)**2 + TMath::CosH(eta)**2 * (0.0131)**2 ) +\
+    ( abs(eta) > 0.8 && abs(eta) <= 1.0 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0006)**2 + TMath::CosH(eta)**2 * (0.0143)**2 ) +\
+    ( abs(eta) > 1.0 && abs(eta) <= 1.2 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0007)**2 + TMath::CosH(eta)**2 * (0.0157)**2 ) +\
+    ( abs(eta) > 1.2 && abs(eta) <= 1.4 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0008)**2 + TMath::CosH(eta)**2 * (0.0169)**2 ) +\
+    ( abs(eta) > 1.4 && abs(eta) <= 1.6 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0007)**2 + TMath::CosH(eta)**2 * (0.0180)**2 ) +\
+    ( abs(eta) > 1.6 && abs(eta) <= 1.8 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0006)**2 + TMath::CosH(eta)**2 * (0.0166)**2 ) +\
+    ( abs(eta) > 1.8 && abs(eta) <= 2.0 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0005)**2 + TMath::CosH(eta)**2 * (0.0205)**2 ) +\
+    ( abs(eta) > 2.0 && abs(eta) <= 2.2 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0005)**2 + TMath::CosH(eta)**2 * (0.0363)**2 ) +\
+    ( abs(eta) > 2.2 && abs(eta) <= 2.4 ) * ( pt > 50 ) * sqrt( pt**2 * TMath::SinH(eta)**2  / (TMath::SinH(eta)**2 + 1) * (0.0008)**2 + TMath::CosH(eta)**2 * (0.0571)**2 )
+}
+
+# See Figure 9.12, pg 484. The resolutions are given for pt inm [1, 10, 100] GeV,
+# so I have to take a little liberty in how to bin things. It looks like the ATLAS
+# resolutions shipped with Delphes are binned with edges [0,5,15,inf] when data was
+# provided for [1,20,200] GeV, for reference.
+set CtgThetaResolutionFormula {
+    ( abs(eta) > 0.0 && abs(eta) <= 0.2 ) * ( pt <= 5 ) * 0.0022 +\
+    ( abs(eta) > 0.2 && abs(eta) <= 0.4 ) * ( pt <= 5 ) * 0.0021 +\
+    ( abs(eta) > 0.4 && abs(eta) <= 0.6 ) * ( pt <= 5 ) * 0.0022 +\
+    ( abs(eta) > 0.6 && abs(eta) <= 0.8 ) * ( pt <= 5 ) * 0.0025 +\
+    ( abs(eta) > 0.8 && abs(eta) <= 1.0 ) * ( pt <= 5 ) * 0.0030 +\
+    ( abs(eta) > 1.0 && abs(eta) <= 1.2 ) * ( pt <= 5 ) * 0.0038 +\
+    ( abs(eta) > 1.2 && abs(eta) <= 1.4 ) * ( pt <= 5 ) * 0.0049 +\
+    ( abs(eta) > 1.4 && abs(eta) <= 1.6 ) * ( pt <= 5 ) * 0.0064 +\
+    ( abs(eta) > 1.6 && abs(eta) <= 1.8 ) * ( pt <= 5 ) * 0.0080 +\
+    ( abs(eta) > 1.8 && abs(eta) <= 2.0 ) * ( pt <= 5 ) * 0.0112 +\
+    ( abs(eta) > 2.0 && abs(eta) <= 2.2 ) * ( pt <= 5 ) * 0.0145 +\
+    ( abs(eta) > 2.2 && abs(eta) <= 2.4 ) * ( pt <= 5 ) * 0.0170 +\
+    ( abs(eta) > 0.0 && abs(eta) <= 0.2 ) * ( pt > 5. && pt <= 50 ) * 0.0008 +\
+    ( abs(eta) > 0.2 && abs(eta) <= 0.4 ) * ( pt > 5. && pt <= 50 ) * 0.0007 +\
+    ( abs(eta) > 0.4 && abs(eta) <= 0.6 ) * ( pt > 5. && pt <= 50 ) * 0.0005 +\
+    ( abs(eta) > 0.6 && abs(eta) <= 0.8 ) * ( pt > 5. && pt <= 50 ) * 0.0006 +\
+    ( abs(eta) > 0.8 && abs(eta) <= 1.0 ) * ( pt > 5. && pt <= 50 ) * 0.0007 +\
+    ( abs(eta) > 1.0 && abs(eta) <= 1.2 ) * ( pt > 5. && pt <= 50 ) * 0.0009 +\
+    ( abs(eta) > 1.2 && abs(eta) <= 1.4 ) * ( pt > 5. && pt <= 50 ) * 0.0010 +\
+    ( abs(eta) > 1.4 && abs(eta) <= 1.6 ) * ( pt > 5. && pt <= 50 ) * 0.0011 +\
+    ( abs(eta) > 1.6 && abs(eta) <= 1.8 ) * ( pt > 5. && pt <= 50 ) * 0.0011 +\
+    ( abs(eta) > 1.8 && abs(eta) <= 2.0 ) * ( pt > 5. && pt <= 50 ) * 0.0013 +\
+    ( abs(eta) > 2.0 && abs(eta) <= 2.2 ) * ( pt > 5. && pt <= 50 ) * 0.0015 +\
+    ( abs(eta) > 2.2 && abs(eta) <= 2.4 ) * ( pt > 5. && pt <= 50 ) * 0.0024 +\
+    ( abs(eta) > 0.0 && abs(eta) <= 0.2 ) * ( pt > 50 ) * 0.0008 +\
+    ( abs(eta) > 0.2 && abs(eta) <= 0.4 ) * ( pt > 50 ) * 0.0006 +\
+    ( abs(eta) > 0.4 && abs(eta) <= 0.6 ) * ( pt > 50 ) * 0.0005 +\
+    ( abs(eta) > 0.6 && abs(eta) <= 0.8 ) * ( pt > 50 ) * 0.0006 +\
+    ( abs(eta) > 0.8 && abs(eta) <= 1.0 ) * ( pt > 50 ) * 0.0006 +\
+    ( abs(eta) > 1.0 && abs(eta) <= 1.2 ) * ( pt > 50 ) * 0.0007 +\
+    ( abs(eta) > 1.2 && abs(eta) <= 1.4 ) * ( pt > 50 ) * 0.0008 +\
+    ( abs(eta) > 1.4 && abs(eta) <= 1.6 ) * ( pt > 50 ) * 0.0007 +\
+    ( abs(eta) > 1.6 && abs(eta) <= 1.8 ) * ( pt > 50 ) * 0.0006 +\
+    ( abs(eta) > 1.8 && abs(eta) <= 2.0 ) * ( pt > 50 ) * 0.0005 +\
+    ( abs(eta) > 2.0 && abs(eta) <= 2.2 ) * ( pt > 50 ) * 0.0005 +\
+    ( abs(eta) > 2.2 && abs(eta) <= 2.4 ) * ( pt > 50 ) * 0.0008
+}
+
+# See Figure 9.14, pg 485.
+set PhiResolutionFormula {
+    ( abs(eta) > -0.0 && abs(eta) <= 0.2 ) * ( pt <= 5 ) * 1.9321 +\
+    ( abs(eta) > 0.2 && abs(eta) <= 0.4 ) * ( pt <= 5 ) * 1.9221 +\
+    ( abs(eta) > 0.4 && abs(eta) <= 0.6 ) * ( pt <= 5 ) * 1.9221 +\
+    ( abs(eta) > 0.6 && abs(eta) <= 0.8 ) * ( pt <= 5 ) * 2.3663 +\
+    ( abs(eta) > 0.8 && abs(eta) <= 1.0 ) * ( pt <= 5 ) * 2.1775 +\
+    ( abs(eta) > 1.0 && abs(eta) <= 1.2 ) * ( pt <= 5 ) * 2.4540 +\
+    ( abs(eta) > 1.2 && abs(eta) <= 1.4 ) * ( pt <= 5 ) * 2.3540 +\
+    ( abs(eta) > 1.4 && abs(eta) <= 1.6 ) * ( pt <= 5 ) * 2.7513 +\
+    ( abs(eta) > 1.6 && abs(eta) <= 1.8 ) * ( pt <= 5 ) * 3.1168 +\
+    ( abs(eta) > 1.8 && abs(eta) <= 2.0 ) * ( pt <= 5 ) * 3.4403 +\
+    ( abs(eta) > 2.0 && abs(eta) <= 2.2 ) * ( pt <= 5 ) * 3.5126 +\
+    ( abs(eta) > 2.2 && abs(eta) <= 2.4 ) * ( pt <= 5 ) * 3.5493 +\
+    ( abs(eta) > -0.0 && abs(eta) <= 0.2 ) * ( pt > 5. && pt <= 50 ) * 0.2824 +\
+    ( abs(eta) > 0.2 && abs(eta) <= 0.4 ) * ( pt > 5. && pt <= 50 ) * 0.2928 +\
+    ( abs(eta) > 0.4 && abs(eta) <= 0.6 ) * ( pt > 5. && pt <= 50 ) * 0.3216 +\
+    ( abs(eta) > 0.6 && abs(eta) <= 0.8 ) * ( pt > 5. && pt <= 50 ) * 0.3037 +\
+    ( abs(eta) > 0.8 && abs(eta) <= 1.0 ) * ( pt > 5. && pt <= 50 ) * 0.3101 +\
+    ( abs(eta) > 1.0 && abs(eta) <= 1.2 ) * ( pt > 5. && pt <= 50 ) * 0.3440 +\
+    ( abs(eta) > 1.2 && abs(eta) <= 1.4 ) * ( pt > 5. && pt <= 50 ) * 0.3877 +\
+    ( abs(eta) > 1.4 && abs(eta) <= 1.6 ) * ( pt > 5. && pt <= 50 ) * 0.3938 +\
+    ( abs(eta) > 1.6 && abs(eta) <= 1.8 ) * ( pt > 5. && pt <= 50 ) * 0.4531 +\
+    ( abs(eta) > 1.8 && abs(eta) <= 2.0 ) * ( pt > 5. && pt <= 50 ) * 0.4699 +\
+    ( abs(eta) > 2.0 && abs(eta) <= 2.2 ) * ( pt > 5. && pt <= 50 ) * 0.4823 +\
+    ( abs(eta) > 2.2 && abs(eta) <= 2.4 ) * ( pt > 5. && pt <= 50 ) * 0.4899 +\
+    ( abs(eta) > -0.0 && abs(eta) <= 0.2 ) * ( pt > 50 ) * 0.0694 +\
+    ( abs(eta) > 0.2 && abs(eta) <= 0.4 ) * ( pt > 50 ) * 0.0727 +\
+    ( abs(eta) > 0.4 && abs(eta) <= 0.6 ) * ( pt > 50 ) * 0.0716 +\
+    ( abs(eta) > 0.6 && abs(eta) <= 0.8 ) * ( pt > 50 ) * 0.0778 +\
+    ( abs(eta) > 0.8 && abs(eta) <= 1.0 ) * ( pt > 50 ) * 0.0778 +\
+    ( abs(eta) > 1.0 && abs(eta) <= 1.2 ) * ( pt > 50 ) * 0.0846 +\
+    ( abs(eta) > 1.2 && abs(eta) <= 1.4 ) * ( pt > 50 ) * 0.0872 +\
+    ( abs(eta) > 1.4 && abs(eta) <= 1.6 ) * ( pt > 50 ) * 0.0828 +\
+    ( abs(eta) > 1.6 && abs(eta) <= 1.8 ) * ( pt > 50 ) * 0.0948 +\
+    ( abs(eta) > 1.8 && abs(eta) <= 2.0 ) * ( pt > 50 ) * 0.0978 +\
+    ( abs(eta) > 2.0 && abs(eta) <= 2.2 ) * ( pt > 50 ) * 0.1217 +\
+    ( abs(eta) > 2.2 && abs(eta) <= 2.4 ) * ( pt > 50 ) * 0.1498
+}
+
+
 # taken from arXiv:1405.6569 fig. 15
 set D0ResolutionFormula {
     ( abs(eta) > 0.0 && abs(eta) <= 0.9 ) * ( pt > 0.1823 && pt <= 0.2227 ) * 0.3543 +\
@@ -104,7 +246,7 @@ set D0ResolutionFormula {
     ( abs(eta) > 1.4 && abs(eta) <= 2.5 ) * ( pt > 73.8967 && pt <= 90.2720 ) * 0.0137 +\
     ( abs(eta) > 1.4 && abs(eta) <= 2.5 ) * ( pt > 90.2720 && pt <= 110.2760 ) * 0.0130 +\
     ( abs(eta) > 1.4 && abs(eta) <= 2.5 ) * ( pt > 110.2760 && pt <= 134.7130 ) * 0.0137 +\
-    ( abs(eta) > 1.4 && abs(eta) <= 2.5 ) * ( pt > 134.7130 ) * 0.0137 
+    ( abs(eta) > 1.4 && abs(eta) <= 2.5 ) * ( pt > 134.7130 ) * 0.0137
 }
 set DZResolutionFormula {
     ( abs(eta) > 0.0 && abs(eta) <= 0.9 ) * ( pt > 0.1823 && pt <= 0.2227 ) * 0.3693 +\
@@ -208,5 +350,5 @@ set DZResolutionFormula {
     ( abs(eta) > 1.4 && abs(eta) <= 2.5 ) * ( pt > 73.8967 && pt <= 90.2720 ) * 0.0816 +\
     ( abs(eta) > 1.4 && abs(eta) <= 2.5 ) * ( pt > 90.2720 && pt <= 110.2760 ) * 0.0820 +\
     ( abs(eta) > 1.4 && abs(eta) <= 2.5 ) * ( pt > 110.2760 && pt <= 134.7130 ) * 0.0814 +\
-    ( abs(eta) > 1.4 && abs(eta) <= 2.5 ) * ( pt > 134.7130 ) * 0.0850 
+    ( abs(eta) > 1.4 && abs(eta) <= 2.5 ) * ( pt > 134.7130 ) * 0.0850
 }
