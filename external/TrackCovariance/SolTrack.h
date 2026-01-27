da536a556babf545be83686b65273853667ab8a9
fbedesch <fbedesch@cern.ch>
2026-01-27 14:21:24 +0100

    Improved speed of Kalman tracking calculation

--- external/TrackCovariance/SolTrack.h
+++ external/TrackCovariance/SolTrack.h
@@ -59,7 +59,8 @@ public:
 	void OldCovCalc(Bool_t Res, Bool_t MS);
 	void CovCalc(Bool_t Res, Bool_t MS);
 	// Track parameter covariance calculation with Kalman filter
-	void KalmanCov(Bool_t Res, Bool_t MS, Double_t mass = 0.13957021);
+	void KalmanCov (Bool_t Res, Bool_t MS, Double_t mass = 0.13957021);
+	void KalmanCovT(Bool_t Res, Bool_t MS, Double_t mass = 0.13957021);
 	// Parameter errors
 	Double_t s_D()    { return TMath::Sqrt(fCov(0, 0)); }
 	Double_t s_phi0() { return TMath::Sqrt(fCov(1, 1)); }
