f5efd3072be7dcb99753218efa75b4cdeac1a9f6
Laurent Forthomme <laurent.forthomme@cern.ch>
2026-03-19 10:49:52 +0100

    Added a new DelphesRun generic reader executable

---
+++ readers/DelphesRun.cpp
@@ -0,0 +1,221 @@
+/*
+ *  Delphes: a framework for fast simulation of a generic collider experiment
+ *  Copyright (C) 2012-2014  Universite catholique de Louvain (UCL), Belgium
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+#include <signal.h>
+
+#include <TApplication.h>
+#include <TFile.h>
+#include <TROOT.h>
+#include <TStopwatch.h>
+
+#include "classes/DelphesClasses.h"
+#include "classes/DelphesFactory.h"
+#include "classes/DelphesReader.h"
+#include "modules/Delphes.h"
+
+#include "ExRootAnalysis/ExRootProgressBar.h"
+#include "ExRootAnalysis/ExRootTreeBranch.h"
+#include "ExRootAnalysis/ExRootTreeWriter.h"
+
+//---------------------------------------------------------------------------
+
+static bool interrupted = false;
+
+void SignalHandler(int sig)
+{
+  interrupted = true;
+}
+
+//---------------------------------------------------------------------------
+
+int main(int argc, char *argv[])
+{
+  char appName[] = "DelphesRun";
+  FILE *inputFile = 0;
+  TStopwatch readStopWatch, procStopWatch;
+  Int_t i, maxEvents, skipEvents;
+  Long64_t length, eventCounter;
+
+  if(argc < 3)
+  {
+    std::cout << " Usage: " << appName << " reader_type config_file output_file"
+              << " [input_file(s)]" << std::endl;
+    std::cout << " config_file - configuration file in Tcl format," << std::endl;
+    std::cout << " output_file - output file in ROOT format," << std::endl;
+    std::cout << " input_file(s) - input file(s) in HepMC format," << std::endl;
+    std::cout << " with no input_file, or when input_file is -, read standard input." << std::endl;
+    return 1;
+  }
+
+  signal(SIGINT, SignalHandler);
+
+  gROOT->SetBatch();
+
+  int appargc = 1;
+  char *appargv[] = {appName};
+  TApplication app(appName, &appargc, appargv);
+
+  try
+  {
+    const auto outputFile = std::make_unique<TFile>(argv[3], "CREATE");
+
+    if(!outputFile)
+    {
+      std::ostringstream message;
+      message << "can't create output file " << argv[3];
+      throw std::runtime_error(message.str());
+    }
+
+    const auto treeWriter = std::make_unique<ExRootTreeWriter>(outputFile.get(), "Delphes");
+
+    ExRootTreeBranch *branchEvent = treeWriter->NewBranch("Event", HepMCEvent::Class()),
+                     *branchWeight = treeWriter->NewBranch("Weight", Weight::Class());
+
+    const auto confReader = std::make_unique<ExRootConfReader>();
+    confReader->ReadFile(argv[2]);
+
+    maxEvents = confReader->GetInt("::MaxEvents", 0);
+    skipEvents = confReader->GetInt("::SkipEvents", 0);
+
+    if(maxEvents < 0)
+    {
+      throw std::runtime_error("MaxEvents must be zero or positive");
+    }
+
+    if(skipEvents < 0)
+    {
+      throw std::runtime_error("SkipEvents must be zero or positive");
+    }
+
+    const auto modularDelphes = std::make_unique<Delphes>("Delphes");
+    modularDelphes->SetConfReader(confReader.get());
+    modularDelphes->SetTreeWriter(treeWriter.get());
+
+    DelphesFactory *factory = modularDelphes->GetFactory();
+    CandidatesCollection allParticleOutputArray = modularDelphes->ExportArray("allParticles"),
+                         stableParticleOutputArray = modularDelphes->ExportArray("stableParticles"),
+                         partonOutputArray = modularDelphes->ExportArray("partons");
+
+    const auto reader = DelphesReaderFactory::Get().Build(argv[1]);
+
+    modularDelphes->InitTask();
+
+    i = 3;
+    do
+    {
+      if(interrupted) break;
+
+      if(i == argc || strncmp(argv[i], "-", 3) == 0)
+      {
+        std::cout << "** Reading standard input" << std::endl;
+        inputFile = stdin;
+        length = -1;
+      }
+      else
+      {
+        std::cout << "** Reading " << argv[i] << std::endl;
+        inputFile = fopen(argv[i], "r");
+
+        if(inputFile == NULL)
+        {
+          std::ostringstream message;
+          message << "can't open " << argv[i];
+          throw std::runtime_error(message.str());
+        }
+
+        fseek(inputFile, 0L, SEEK_END);
+        length = ftello(inputFile);
+        fseek(inputFile, 0L, SEEK_SET);
+
+        if(length <= 0)
+        {
+          fclose(inputFile);
+          ++i;
+          continue;
+        }
+      }
+
+      reader->SetInputFile(inputFile);
+
+      ExRootProgressBar progressBar(length);
+
+      // Loop over all objects
+      eventCounter = 0;
+      treeWriter->Clear();
+      modularDelphes->Clear();
+      reader->Clear();
+      readStopWatch.Start();
+      while((maxEvents <= 0 || eventCounter - skipEvents < maxEvents) && reader->ReadBlock(factory, allParticleOutputArray, stableParticleOutputArray, partonOutputArray) && !interrupted)
+      {
+        if(reader->EventReady())
+        {
+          ++eventCounter;
+
+          readStopWatch.Stop();
+
+          if(eventCounter > skipEvents)
+          {
+            procStopWatch.Start();
+            modularDelphes->ProcessTask();
+            procStopWatch.Stop();
+
+            reader->AnalyzeEvent(branchEvent, eventCounter, &readStopWatch, &procStopWatch);
+            reader->AnalyzeWeight(branchWeight);
+
+            treeWriter->Fill();
+
+            treeWriter->Clear();
+          }
+
+          modularDelphes->Clear();
+          reader->Clear();
+          allParticleOutputArray->clear();
+          stableParticleOutputArray->clear();
+          partonOutputArray->clear();
+
+          readStopWatch.Start();
+        }
+        progressBar.Update(ftello(inputFile), eventCounter);
+      }
+
+      fseek(inputFile, 0L, SEEK_END);
+      progressBar.Update(ftello(inputFile), eventCounter, kTRUE);
+      progressBar.Finish();
+
+      if(inputFile != stdin) fclose(inputFile);
+
+      ++i;
+    } while(i < argc);
+
+    modularDelphes->FinishTask();
+    treeWriter->Write();
+
+    std::cout << "** Exiting..." << std::endl;
+
+    return 0;
+  }
+  catch(const std::runtime_error &e)
+  {
+    std::cerr << "** ERROR: " << e.what() << std::endl;
+    return 1;
+  }
+}
